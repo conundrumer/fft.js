b1db2658a4f2605350e2a5fe4eafbf25268f8ab9
David Lu <davidelu93@gmail.com>
2014-07-20 01:54:57 -0400

    Add generalized testing

--- test.js
+++ test.js
@@ -27,9 +27,6 @@ function kahanDiff(x, xOffset, xStride, y, yOffset, yStride, n, run) {
 function toArray(a) {
 	return Array.prototype.slice.call(a)
 }
-function half(n) {
-	return n/2
-}
 
 function testSimpleRealFFT(debug) {
 	var N = 4;
@@ -69,10 +66,11 @@ function testSimpleRealFFT(debug) {
 		}
 	}
 }
+
 function testSimpleRealFFT2(debug) {
 	var N = 8
 	var s = Math.SQRT1_2
-	var fft = new FFT.real(N, false)
+	var fft = new FFT.real(N)
 	var ifft = new FFT.inverse.real(N)
 	var io = [
 		// time       //freq (dc, nyquist, bin1_r/bin3_r, bin1_i/-bin3_i)
@@ -108,16 +106,95 @@ function testSimpleRealFFT2(debug) {
 		}
 	}
 }
-console.log("n = 4")
-testSimpleRealFFT();
-console.log("n = 8")
-testSimpleRealFFT2();
 
-function testRealFFT(n, debug) {
-	// DC
-	// impulse response
-	// impulse response delayed 1 sample
-	// cos
-	// sin
-	// nyquist
+function fill(output, input) {
+	for (var i = 0; i < input.length; i++) {
+		output[i] = input[i]
+	}
 }
+
+function generateTestSignals(N) {
+	return [
+		['DC', function(){return 1}],
+		['dirac', function(n){return n === 0 ? 1 : 0}],
+		['dirac_shift1', function(n) {return n === 1 ? 1 : 0}],
+		['cosine', function(n, N) {return Math.cos(2*Math.PI*n/N)}],
+		['sine', function(n, N) {return Math.sin(2*Math.PI*n/N)}],
+		['nyquist', function(n) {return n%2}],
+		['white_noise', function() {return Math.random()}]
+	].map(function(param) {
+		return new Signal(param[0], param[1], N)
+	})
+}
+
+var epsilon = 1e-10
+function testRealFFT(N, debug) {
+	if (debug) {
+		console.log("Testing N=",N)
+	}
+	var fft = new FFT.real(N)
+	var ifft = new FFT.inverse.real(N)
+	var input = new Float64Array(N)
+	var output = new Float64Array(N)
+	var signals = generateTestSignals(N)
+	var diff
+	signals.forEach(function(signal) {
+		fill(input, signal.time)
+		fft.simple(output, input)
+		diff = kahanDiff(signal.freq, 0, 1, output, 0, 1, 1, 1) // normalized
+		if ( isNaN(diff) || diff > epsilon) {
+			console.log("Forward FFT Failed:", signal.name, N, ", Error: ", diff)
+			if (debug) {
+				console.log("expected:", signal.freq)
+				console.log("result:", toArray(output))
+			}
+		}
+
+		fill(input, signal.freq)
+		ifft.simple(output, input)
+		diff = kahanDiff(signal.time, 0, 1, output, 0, 1, N, 1) // not normalized
+		if ( isNaN(diff) || diff > epsilon) {
+			console.log("Inverse FFT Failed:", signal.name, N, ", Error: ", diff)
+			if (debug) {
+				console.log("expected:", signal.time)
+				console.log("result:", toArray(output))
+			}
+		}
+	})
+}
+
+// dft
+function makeBin(time, k, N) {
+	return time.map(function(x_n, n) {
+		var phase = -2 * Math.PI * k * n / N
+		return [x_n*Math.cos(phase), x_n*Math.sin(phase)]
+	}).reduce(function(a,b){
+		return [a[0]+b[0],a[1]+b[1]]
+	})
+}
+
+function Signal(name, fn, N) {
+	this.name = name;
+	this.time = []
+	for (var n = 0; n < N; n++) {
+		this.time[n] = fn(n, N)
+	}
+	this.freq = []
+	for (var k = 0; k <= N/2; k++) {
+		var bin = makeBin(this.time, k, N)
+		this.freq[2*k] = bin[0]
+		this.freq[2*k+1] = bin[1]
+	}
+	this.freq[1] = this.freq[N]
+	this.freq.length = N
+}
+
+console.log("testing n = 4")
+testSimpleRealFFT();
+console.log("testing n = 8")
+testSimpleRealFFT2();
+console.log("testing n = 4 to 128, evens")
+for (var i = 4; i <= 128; i += 2) {
+	testRealFFT(i, false)
+}
+console.log("done")
